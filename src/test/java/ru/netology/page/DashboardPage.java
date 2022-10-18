5b982de6fcbd6b66758fcb5a90d28a26cf9fe760
Kris Tsvetkova <creedance666@gmail.com>
2022-10-18 14:19:31 +0300

    Initial commit

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,32 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import lombok.val;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private ElementsCollection cards = $$(".list__item div");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+    public DashboardPage() {
+    }
+
+    public int getFirstCardBalance() {
+        val text = cards.first().text();
+        return extractBalance(text);
+    }
+
+    public int getSecondCardBalance() {
+        val text = cards.get(1).text();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+}
